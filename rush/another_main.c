53cb427f3b03cad7f1bf0dba2734e67dbdacc744
Panu Nimwatana <pnimwata@z1t1c3.42bangkok.com>
2022-01-23 19:07:25 +0700

    edit another_main.c

--- rush/another_main.c
+++ rush/another_main.c
@@ -6,7 +6,7 @@
 /*   By: pnimwata <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 14:17:24 by pnimwata          #+#    #+#             */
-/*   Updated: 2022/01/23 00:04:48 by pnimwata         ###   ########.fr       */
+/*   Updated: 2022/01/23 19:03:09 by pnimwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,89 +16,87 @@
 
 int	putnbr(char *c)
 {
+	int		index;
 	char	*proto_num;
-	int	index;
 
 	proto_num = "0123456789";
 	index = 0;
 	while (*(proto_num + index) != '\0')
 	{
-		if(*c == *(proto_num + index))
+		if (*c == *(proto_num + index))
 			return (index);
 		index++;
 	}
 	return (-1);
 }
 
+int	is_nbr(char *num)
+{
+	if (*num >= '0' && *num <= '9')
+		return (1);
+	return (0);
+}
+
 int	count_char_num(char *num)
 {
-	int	count = 0;
-	int	index = 0;
+	int	count;
+	int	index;
 
+	count = 0;
+	index = 0;
 	while (*(num + index) != '\0')
 	{
-		if (*(num + index) >= '0' && *(num + index) <= '9')
+		if (is_nbr(num))
 			count++;
 		index++;
 	}
 	return (count);
 }
-/*
+
 int	*convert_to_arr(char *str, int *max_num)
 {
-	int	*new_arr;
-	int	index;
+	int		*new_arr;
+	int		new_arr_i;
+	int		index;
+	char	*c;
 
 	index = 0;
-	while (index < max_num)
+	new_arr = malloc(*max_num * 4);
+	new_arr_i = 0;
+	while (str[index] != '\0')
 	{
-		
+		c = str + index;
+		if (is_nbr(c) == 1)
+		{
+			*(new_arr + new_arr_i) = putnbr(c);
+			new_arr_i++;
+		}
+		else if (!(is_nbr(c) == 1) && *c != ' ')
+		{
+			write(1, "error format\n", 12);
+			return (0);
+		}
+		index++;
 	}
 	return (new_arr);
-}*/
+}
 
 int	main(int counter, char **vector)
 {
 	int	*arr_num;
 	int	max_vector;
-	int	max_vector_nbr;
+	int	max_table;
 
-	max_vector_nbr = count_char_num(*(vector + 1));
-	arr_num = malloc(max_vector_nbr * 4);
+	max_table = count_char_num(*(vector + 1)) / 4;
 	max_vector = sizeof (*(vector + 1));
-	if (counter != 2 )
+	if (counter != 2)
 	{
 		write(1, "error\n", 6);
 		return (0);
 	}
-	int 	i ;
-	int	arr_i;
-	char	*c;
-
-	i = 0;
-	arr_i = 0;
-	while (i < max_vector_nbr * 2 - 1) 
-	{
-		c = *(vector + 1) + i; // 4
-		if (*c == ' ')
-		{
-			i++;
-			continue;
-		}
-		if (*c >= '0' && *c <= '9')
-		{
-			*(arr_num + arr_i) = putnbr(c);
-			arr_i++;
-		}
-		else
-		{
-			write(1, "error format\n", 12);
-			return (0);
-		}
-		i++;	
-	}
-	i = 0;
-	while (i < max_vector_nbr)
+	arr_num = convert_to_arr(*(vector + 1), &max_table);
+	int i = 0;
+	while (i < max_table * 4)
 	{
 		printf("%d", *(arr_num + i));
 		i++;
